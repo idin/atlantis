454da11ef3c39b9288a45245066775e472ab2bdd
Idin <git@idin.net>
2021-03-10 14:28:28 -0800

    validation update

--- atlantis/ds/validation/_TrainingTestContainer.py
+++ atlantis/ds/validation/_TrainingTestContainer.py
@@ -1,13 +1,18 @@
 
 from pandas import DataFrame
 from ...exceptions import MissingArgumentError
-from ._Evaluation import TestResult, TrainingResult, Evaluation
+from ._Evaluation import Evaluation
+from ..evaluation import evaluate_regression, evaluate_classification
 from ._DataContainer import get_display_function, DataContainer
+from ...time.progress import ProgressBar
 
 
 class TrainingTestContainer(DataContainer):
-	def __init__(self, data, training_indices, test_indices, x_columns=None, y_column=None):
-		super().__init__(data=data, x_columns=x_columns, y_column=y_column)
+	def __init__(
+			self, data, training_indices, test_indices, x_columns=None, y_column=None,
+			sort_columns=None
+	):
+		super().__init__(data=data, x_columns=x_columns, y_column=y_column, sort_columns=sort_columns)
 		self._training_indices = training_indices
 		self._test_indices = test_indices
 
@@ -32,6 +37,72 @@ class TrainingTestContainer(DataContainer):
 		:rtype: DataFrame
 		"""
 		return self._data.iloc[self._test_indices]
+
+	def _evaluate_estimator(self, estimator, evaluation_type, x_columns=None, y_column=None):
+		y_column = y_column or self._y_column
+		if y_column is None:
+			raise ValueError(f'y_column should be provided')
+		x_columns = x_columns or self._x_columns
+		if x_columns is None:
+			x_columns = [col for col in self.columns if col != y_column]
+
+
+	def _evaluate(self, estimators, evaluation_type, x_columns=None, y_column=None, echo=1):
+		if isinstance(estimators, (list, tuple)):
+			estimators = {(i + 1): x for i, x in enumerate(estimators)}
+
+		if isinstance(estimators, dict):
+			records = []
+			progress_bar = ProgressBar(total=len(estimators), echo=echo)
+			progress_amount = 0
+			for estimator_id, estimator in estimators.items():
+				progress_bar.show(amount=progress_amount, text=f'evaluating estimator {estimator_id}')
+				evaluation = self._evaluate_estimator(
+					estimator=estimator, evaluation_type=evaluation_type,
+					x_columns=x_columns, y_column=y_column
+				)
+				records.append({
+					'estimator_id': estimator_id,
+					'estimator_name': estimator.__class__.__name__,
+					**evaluation
+				})
+				progress_amount += 1
+			progress_bar.show(amount=progress_amount, text=f'evaluation complete!')
+			return DataFrame.from_records(records)
+
+		else:
+			return self._evaluate_estimator(
+				estimator=estimators, evaluation_type=evaluation_type,
+				x_columns=x_columns, y_column=y_column
+			)
+
+	def evaluate_regression(self, estimators, x_columns=None, y_column=None, echo=1):
+		"""
+		evaluates one regressor or more and returns a dictionary or a dataframe of the results
+		:param estimators: one or more regressors
+		:param x_columns: the x columns (independent variables)
+		:param y_column: the y column (dependent variable)
+		:param echo: to show progress or not
+		:rtype: dict or DataFrame
+		"""
+		return self._evaluate(
+			estimators=estimators, evaluation_type='regression', x_columns=x_columns, y_column=y_column,
+			echo=echo
+		)
+
+	def evaluate_classification(self, estimators, x_columns=None, y_column=None, echo=1):
+		"""
+		evaluates one classifier or more and returns a dictionary or a dataframe of the results
+		:param estimators: one or more classifiers
+		:param x_columns: the x columns (independent variables)
+		:param y_column: the y column (dependent variable)
+		:param echo: to show progress or not
+		:rtype: dict or DataFrame
+		"""
+		return self._evaluate(
+			estimators=estimators, evaluation_type='classification', x_columns=x_columns, y_column=y_column,
+			echo=echo
+		)
 
 	def display(self, p=None, prefix='', function=None):
 		if function is None:
--- atlantis/ds/validation/_train_and_test.py
+++ atlantis/ds/validation/_train_and_test.py
@@ -1,7 +1,33 @@
 from ..evaluation import evaluate_regression, evaluate_classification
 
 
-def evaluate(estimator, evaluation_type, training_data, test_data, x_columns, y_column, sort_columns):
+def train_and_test(
+		estimator, evaluation_type, training_data, test_data, x_columns, y_column, sort_columns,
+		ignore_missing_y=False, ignore_missing_x=False, replace_missing_x='mean'
+):
+	training_data = training_data[x_columns + [y_column]]
+
+	if ignore_missing_y and ignore_missing_y:
+		training_data = training_data.drop_na()
+		test_data = test_data.drop_na()
+
+	elif ignore_missing_y:
+		training_data = training_data[training_data[y_column].notna()]
+		test_data = test_data[training_data[y_column].notna()]
+
+	elif ignore_missing_x:
+		training_data = training_data[~training_data[x_columns].isnull().any(axis=1)]
+		test_data = test_data[~test_data[x_columns].isnull().any(axis=1)]
+
+	if not ignore_missing_x:
+		if replace_missing_x == 'mean':
+			training_data[x_columns] = training_data[x_columns].fillna(training_data[x_columns].mean())
+			test_data[x_columns] = test_data[x_columns].fillna(test_data[x_columns].mean())
+
+		elif replace_missing_x == 'median':
+			training_data[x_columns] = training_data[x_columns].fillna(training_data[x_columns].median())
+			test_data[x_columns] = test_data[x_columns].fillna(test_data[x_columns].median())
+
 	estimator.fit(X=training_data[x_columns], y=training_data[y_column])
 	actual = test_data[y_column]
 	predicted = estimator.predict(test_data[x_columns])
